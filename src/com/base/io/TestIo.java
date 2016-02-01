ed8599ef4268e90eb2af3733c4a270bdaaf7c263
haowsms <316397408@qq.com>
2016-02-01 22:44:26 +0800

    modify

--- src/com/base/io/TestIo.java
+++ src/com/base/io/TestIo.java
@@ -1,5 +1,14 @@
 package com.base.io;
 
 public class TestIo {
-
+	
+	public int test(){  
+        int count = 1;  
+        try{  
+                 return ++count;  
+        }finally{  
+                 return ++count;  
+        }  
+}  
+	
 }
