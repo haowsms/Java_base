0b382a37502c38e6fa22a219320a0143b8bb1fcf
haowsms <316397408@qq.com>
2016-02-02 00:42:41 +0800

    commit

--- src/com/base/io/TestIo.java
+++ src/com/base/io/TestIo.java
@@ -2,7 +2,7 @@ package com.base.io;
 
 public class TestIo {
 	
-	public int test(){  
+	public int test1(){  
         int count = 1;  
         try{  
                  return ++count;  
@@ -11,4 +11,18 @@ public class TestIo {
         }  
 }  
 	
+//以上代码最终返回值是：3  
+public int test2(){  
+        int count = 1;  
+        try{  
+                 return ++count;  
+        }finally{  
+                 return count++;  
+        }  
+}  
+	public static void main(String[] args) {
+		TestIo t = new TestIo();
+		System.out.println(t.test1());
+		System.out.println(t.test2());
+	}
 }
